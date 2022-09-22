0573f8bc4deac196e62bcca6fd8d257cb6a59f58
maxence.de-torquat-de-la-coulerie <maxence.de-torquat-de-la-coulerie@epita.fr>
2022-09-22 18:31:01 +0200

    push find_motive v1

---
+++ FIND_MOTIVE/findmotive.cpp
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <assert.h>
+#include <stddef.h>
+
+#include "findmotive.hpp"
+
+int getLength(const char *str)
+{
+    int count = 0;
+    while (str[count])
+    {
+        count++;
+    }
+    return count;
+}
+
+
+int myfindmotive(char* file, int count)
+{
+	int nb = 0;
+
+	for(int i = 0; i < getLength(path); i++)
+	{
+		//TODO
+	}
+
+	return nb;
+}
---
+++ FIND_MOTIVE/main.cpp
@@ -0,0 +1,11 @@
+#include<findmotive.cpp>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+	{
+		std::cout << "The file *input-file-path* could not be opened" << std::endl;
+        return 1;
+    }
+	return 0;
+}
