9f83d36818df15d38af9d6e6931b1e2e6189442c
maxence.de-torquat-de-la-coulerie <maxence.de-torquat-de-la-coulerie@epita.fr>
2022-09-26 22:13:42 +0200

    push vf is_prime

--- IS_PRIME/isprime.cpp
+++ IS_PRIME/isprime.cpp
@@ -1,8 +1,18 @@
-bool isprime(long long n)
+#include<iostream>
+#include "isprime.h"
+
+bool isprime(int n)
 {
-	if (n <= 0)
+	if (n < 2)
+		return false;
+	if (n == 2)
+		return true;
+
+	for (int i = 3; i < n; i+=2)
 	{
-		std::cout << n << "is a prime : False" << std::endl;
+		if (n % i == 0)
+			return false;
 	}
 
+	return true;
 }
--- IS_PRIME/main.cpp
+++ IS_PRIME/main.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
-#include "isprime.hpp"
+#include "isprime.h"
+
+int main()
+{
+	int number;
+
+	while (std::cin >> number)
+	{
+		if (!isprime(number))
+			std::cout << number << " is a prime: False\n" << std::endl;
+		else
+			std::cout << number << " is a prime: True\n" << std::endl;
+	}
+
+	return 0;
+}
