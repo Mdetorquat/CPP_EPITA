cd507737c8dcd4dacf295c7cad61bafab78a87b2
maxence.de-torquat-de-la-coulerie <maxence.de-torquat-de-la-coulerie@epita.fr>
2022-10-13 18:23:19 +0200

    push v2 cartesian_vector

--- cartesian_vector_param/vector.cpp
+++ cartesian_vector_param/vector.cpp
@@ -1,67 +1,132 @@
 #include "vector.hh"
 
-using namespace std;
+#include <iostream>
+#include <ostream>
+
+size_t Vector::get_size() const
+{
+    return size;
+}
+
 
 Vector::Vector()
 {
-	x = 0;
-	y = 0;
-
+	size = NDIM;
+	for (size_t i = 0; i < size; i++)
+	{
+		data[i] = 0;
+	}
 }
 
-Vector::Vector(int i, int j) : x{i}, y{j}
+Vector::Vector(std::initializer_list<value> list)
 {
-	
+	size = list.size();
+    size_t i = 0;
+    for (auto &elem : list)
+    {
+        data[i++] = elem;
+    }
 }
 
 // Operation addition
-Vector Vector::operator+=(const Vector& rhs)
+Vector &Vector::operator+=(const Vector &rhs)
 {
-	x+=rhs.x;
-	y+=rhs.y;
-
+	for (size_t i = 0; i < rhs.get_size(); i++)
+	{
+		data[i] = data[i] + rhs[i];
+	}
 	return *this;
 }
 
-Vector& Vector::operator+(const Vector& rhs)
+Vector Vector::operator+(const Vector &rhs)
 {
-	return Vector(x+rhs.x, y+rhs.y);
+	auto v = Vector();
+	for (size_t i = 0; i < rhs.get_size(); i++)
+	{
+		v[i] = rhs[i] + (*this)[i];
+	}
+	return v;
 }
 
 // Operation soustraction
-Vector& Vector::operator-=(const Vector& rhs)
+Vector &Vector::operator-=(const Vector &rhs)
 {
-	x-=rhs.x;
-	y-=rhs.y;
-
+	for (size_t i = 0; i < rhs.get_size(); i++)
+	{
+		data[i] = data[i] - rhs[i];
+	}
 	return *this;
 }
 
-Vector& Vector::operator-(const Vector& rhs)
+Vector Vector::operator-(const Vector& rhs)
 {
-	return Vector(x-rhs.x, y-rhs.y);
+	auto v = Vector();
+    for (size_t i = 0; i <  rhs.get_size(); i++)
+    {
+        v[i] = rhs[i] - (*this)[i];
+    }
+    return v;
 }
 
 
 // Operation multiplication
-Vector& Vector::operator*=(const Vector& rhs)
+value Vector::operator*(const Vector &rhs)
 {
-	x*=rhs.x;
-	y*=rhs.y;
-
-	return *this;
+    value v = 0;
+    for (size_t i = 0; i <  rhs.get_size(); i++)
+    {
+        v += rhs[i] * (*this)[i];
+    }
+    return v;
 }
 
-Vector& Vector::operator*(const Vector& rhs, const value val)
+value Vector::operator[](size_t val) const
 {
-	x *= i;
-	y *= i;
-	
-	return *this;
+	return data[val];
+}
+
+value& Vector::operator[](size_t val)
+{
+	return data[val];
 }
 
 
-value Vector::operator[](const value val)
+
+Vector operator*(Vector& rhs, const value val)
 {
-	return coordonnees[val];
+	auto v = Vector();
+    for (size_t i = 0; i <  rhs.get_size(); i++)
+    {
+        v[i] = rhs[i] * val;
+    }
+    return v;
 }
+
+Vector operator*=(Vector &rhs, const value val)
+{
+    for (size_t i = 0; i <  rhs.get_size(); i++)
+    {
+        rhs[i] = rhs[i] * val;
+    }
+    return rhs;
+}
+
+Vector operator+=(Vector &rhs, const value val)
+{
+    for (size_t i = 0; i <  rhs.get_size(); i++)
+    {
+        rhs[i] = rhs[i] + val;
+    }
+    return rhs;
+}
+
+std::ostream& operator<<(std::ostream &os, const Vector& rhs)
+{
+    os << "{";
+    for (int i = 0; i < rhs.get_size(); i++)
+    {
+        os << rhs[i] << (i == rhs.get_size() - 1 ? "" : ",");
+    }
+    return os << "}";
+}
+
--- cartesian_vector_param/vector.hh
+++ cartesian_vector_param/vector.hh
@@ -15,31 +15,33 @@ public:
 
 // Add suitable constructors
     Vector();
-    Vector(int x, int y);
+    Vector(std::initializer_list<value> list);
     // possibly more
 
 // Public Member functions here
-    Vector& operator+=(const Vector& rhs);
-	Vector operator+(const Vector& rhs);
-	Vector& operator-=(const Vector& rhs);
-	Vector operator-(const Vector& rhs);
-	value operator*(const Vector& rhs);
-	value Vector::operator[](const value val);
+    Vector &operator+=(const Vector &rhs);
+	Vector operator+(const Vector &rhs);
+	Vector &operator-=(const Vector &rhs);
+	Vector operator-(const Vector &rhs);
+	value operator*(const Vector &rhs);
+	size_t get_size() const;
+	value operator[](size_t index) const;
+	value& operator[](size_t index);
+
     // More to go
 
 private:
-// Private Member functions here
-
-// Notre vecteur en fonction de NDIM
-value coordonnees[NDIM];
+	// Private Member functions here
+	value data[NDIM];
+	size_t size;
 
 // Member variables are ALWAYS private, and they go here
+
 };
 
 // Nonmember function operators go here
 
-std::ostream& operator<<(std::ostream& ost, const Vector& rhs);
-//Vector& operator+(const Vector& vect1, const Vector vect2);
-Vector operator*(const Vector& rhs, const value val);
-Vector operator*=(const Vector& rhs, const value val);
-Vector operator+=(const Vector& rhs, const value val);
+std::ostream& operator<<(std::ostream &os, const Vector &rhs);
+Vector operator*(const Vector &rhs, const value val);
+Vector operator*=(const Vector &rhs, const value val);
+Vector operator+=(const Vector &rhs, const value val);
